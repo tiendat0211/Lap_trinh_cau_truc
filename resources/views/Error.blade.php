a4cf8eb7d1f4473eb2f00219b01f402239c7bf96
tiendat0211 <79643933+tiendat0211@users.noreply.github.com>
2021-12-27 01:12:26 +0700

    Update Css

--- resources/views/Error.blade.php
+++ resources/views/Error.blade.php
@@ -21,8 +21,8 @@
         </style>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
-    <body class="antialiased">
-        <div class="main-content">
+    <body >
+        <div class="container">
 
         @if (session('error'))
         
